44d9a30115e469df3a9806ca3ac52ee9bf0b229c
Stronauta <samircastillop@gmail.com>
2023-10-02 18:24:52 -0400

    Se creo la bll junto a las funciones Exite,Insectar y modoficar. Tambien, se agregaron algunos datos al modelo

---
+++ BLL/AportesBLL.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Samir_AP1_P1.DAL;
+using Samir_AP1_P1.Models;
+
+namespace Samir_AP1_P1.BLL
+{
+	public class AportesBLL
+	{
+		private Context _contexto;
+
+		public AportesBLL(Context contexto)
+		{
+			_contexto = contexto;
+		}
+
+		public bool Existe(int aportesId)
+		{
+			return _contexto.Aportes.Any(a => a.AporteID == aportesId);
+		}
+
+		public bool Insectar(Aportes aportes)
+		{
+			_contexto.Aportes.Add(aportes);
+			return _contexto.SaveChanges() > 0;
+		}
+
+		public bool Modificar(Aportes aportes)
+		{
+			_contexto.Entry(aportes).State = EntityState.Modified;
+			return _contexto.SaveChanges() > 0;
+		}
+
+	}
+}
--- Models/Aportes.cs
+++ Models/Aportes.cs
@@ -8,9 +8,14 @@ namespace Samir_AP1_P1.Models
 		[Key]
 		public int AporteID { get; set; }
 		[Required]
+		public DateTime Fecha { get; set; } = DateTime.Now;
+		[Required]
 		public string Persona { get; set; }
+		[Required]
+		public string Descripcion { get; set; }
 		[Required]
 		[Range(minimum: 1, maximum: 10000000, ErrorMessage = "El monto debe ser mayor a 0")]
 		public decimal Monto { get; set; }
+
 	}
 }
