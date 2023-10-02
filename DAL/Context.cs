21fd5c468e0f0b3f81c0fec1f03271225a787758
Stronauta <samircastillop@gmail.com>
2023-10-02 18:15:50 -0400

    Agregando el modelo y terminando el modelo

--- DAL/Context.cs
+++ DAL/Context.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Samir_AP1_P1.Models;
 using System.Security.Cryptography.X509Certificates;
-
 namespace Samir_AP1_P1.DAL
 {
-    public class Context: DbContext
+    public class Context : DbContext
     {
-        public Context(DbContextOptions<Context> options):base(options) 
+        public DbSet<Aportes> Aportes { get; set; }
+    public Context(DbContextOptions<Context> options):base(options) 
         {
             
         }
---
+++ Models/Aportes.cs
@@ -0,0 +1,16 @@
+﻿using Samir_AP1_P1.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Samir_AP1_P1.Models
+{
+	public class Aportes
+	{
+		[Key]
+		public int AporteID { get; set; }
+		[Required]
+		public string Persona { get; set; }
+		[Required]
+		[Range(minimum: 1, maximum: 10000000, ErrorMessage = "El monto debe ser mayor a 0")]
+		public decimal Monto { get; set; }
+	}
+}
